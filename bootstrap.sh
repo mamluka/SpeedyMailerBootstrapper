56be08b5f8b66e10b9ebba024a6b00b83fa5075f
davidmz <david.mazvovsky@gmail.com>
2012-12-28 21:19:34 +0200

    first

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+gem install rake
+gem install albacore
+gem install nokogiri
+gem install sys-proctable --platform mswin32
+gem install thor
+
+mkdir /c/SpeedyMailer
+cd /c/SpeedyMailer
+
+mkdir Source
+cd Source
+
+
