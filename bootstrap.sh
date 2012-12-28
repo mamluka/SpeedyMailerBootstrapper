7441e7a188f36ac9066126a156abfdbc370cf68b
davidmz <david.mazvovsky@gmail.com>
2012-12-28 22:01:49 +0200

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,16 @@
+if [ ! -d "/c/SpeedyMailer/Source" ]; then
+	mkdir -p /c/SpeedyMailer/Source
+	cd /c/SpeedyMailer/Source
+	
+	git clone https://github.com/mamluka/SpeedyMailer.git
+fi
+
+cd /c/SpeedyMailer/Source/SpeedyMailer
+
+git pull
+
 gem install rake
 gem install albacore
 gem install nokogiri
 gem install sys-proctable --platform mswin32
 gem install thor
-
-mkdir /c/SpeedyMailer
-cd /c/SpeedyMailer
-
-mkdir Source
-cd Source
-
-
